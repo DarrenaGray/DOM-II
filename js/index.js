0e221dea1969b5eaa9fa01979a02a029b10ee03c
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-01-29 16:02:57 -0500

    Started outline of JSON data

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+const siteContent = {
+    "nav": {
+        "nav1": "Home",
+        "nav2": "About Us",
+        "nav3": "Blog",
+        "nav4": "Contact"
+    },
+    "intro": {
+
+    },
+    "content-section": {
+
+    },
+    "content-destination": {
+
+    },
+    "content-pick": {
+
+    },
+    "footer": {
+
+    },
+};
