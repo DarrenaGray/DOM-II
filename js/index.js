8518a594f7a7baacf96dcb06f58f8600f17ddbd9
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-01-29 17:26:43 -0500

    Created DOM selectors for index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const nav = document.querySelectorAll('.nav-link');
+console.log(nav);
+
+const h4Headers = document.querySelectorAll('h4');
+console.log(h4Headers);
+const h2Headers = document.querySelectorAll('h2');
+console.log(h2Headers);
+const introContent = document.querySelector('p');
+console.log(introContent); 
+const middleContent = document.querySelectorAll('p');
+console.log(middleContent);
+const bottomContent = document.querySelectorAll('p');
+console.log(bottomContent);
+const button = document.querySelectorAll('.btn');
+console.log(button);
