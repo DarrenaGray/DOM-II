4a32e23a7ef23fb13bb7b4ed1c41c52331f62a84
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-01-30 18:45:27 -0500

    Added a few more event listeners to index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Your code goes here
 const nav = document.querySelectorAll('.nav-link');
 console.log(nav);
-const introContent = document.querySelector('p');
-console.log(introContent);
+const header = document.querySelector('h1');
+console.log(header);
 const h2Headers = document.querySelectorAll('h2');
 console.log(h2Headers);
 const h4Headers = document.querySelectorAll('h4');
@@ -12,40 +12,59 @@ console.log(middleContent);
 const bottomContent = document.querySelectorAll('p');
 console.log(bottomContent);
 const button = document.querySelectorAll('.btn');
-console.log(button);
 
 
-nav.forEach(element => {
-    element.addEventListener('mouseenter', element => {
-        console.log(`This element: ${element.type}`)
-        element.target.style.color = "red";
-    });
-    // element[0].target;
+nav.forEach(event => {
+    event.addEventListener('mouseover', event => {
+        // console.log(`This event: ${event.type}`)
+        event.target.style.color = "red"; 
+        setTimeout (() => {
+            event.target.style.color = '';
+           }, 500);
+    }, false);
 });
 
-introContent.addEventListener('click', element => {
-    console.log(`This is ${element.type}`);
+header.addEventListener('dblclick', event => {
+    console.log(`${event.type}`);
+    event.target.innerHTML = '100px';
 })
 
-h2Headers.forEach(element => {
-    element.addEventListener('wheel', element => {
-        console.log(`${element.target}`);
+h2Headers.forEach(event => {
+    event.addEventListener('click', event => {
+        console.log(`${event.type}`);
+        event.target.innerHTML = 'HAVE FUN!';
     })
 })
 
-h4Headers.forEach(element => {
-    element.addEventListener('select', element => {
-        console.log(`This element ${element.type}`);
+h4Headers.forEach(event => {
+    event.addEventListener('select', event => {
+        console.log(`This event ${event.type}`);
     })
 })
 
-middleContent.forEach(element => {
-    element.addEventListener('blur', element => {
-    
+middleContent.forEach(event => {
+    event.addEventListener('blur', event => {
+        // console.log(`${event.target, true}`);
+    })
 })
 
-// button.forEach(element => {
-//     // element.addEventListener('mousemove', element => {
-//     //     console.log(`This element ${element.type}`)
-//     // })
-// })
+button.forEach(event => {
+    event.addEventListener('mouseenter', event => {
+        console.log(`This event ${event.type}`);
+        event.target.style.backgroundColor = "black"; 
+        setTimeout(() => {
+            event.target.style.backgroundColor = '';
+        }, 1000);   
+    },)
+})
+
+
+button.forEach(event => {
+    event.addEventListener('mouseout', event => {
+        event.target.style.color = "red";
+        setTimeout(() => {
+            event.target.style.color = '';
+        },1000);
+    })
+})
+
