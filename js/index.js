18256785ce8457d28982c73a31224619bf7b09be
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-01-29 19:34:57 -0500

    Added different events and logged to make sure that they're registering now  need to figure out how to use each event

--- js/index.js
+++ js/index.js
@@ -1,16 +1,51 @@
 // Your code goes here
 const nav = document.querySelectorAll('.nav-link');
 console.log(nav);
-
-const h4Headers = document.querySelectorAll('h4');
-console.log(h4Headers);
+const introContent = document.querySelector('p');
+console.log(introContent);
 const h2Headers = document.querySelectorAll('h2');
 console.log(h2Headers);
-const introContent = document.querySelector('p');
-console.log(introContent); 
+const h4Headers = document.querySelectorAll('h4');
+console.log(h4Headers);
 const middleContent = document.querySelectorAll('p');
 console.log(middleContent);
 const bottomContent = document.querySelectorAll('p');
 console.log(bottomContent);
 const button = document.querySelectorAll('.btn');
 console.log(button);
+
+
+nav.forEach(element => {
+    element.addEventListener('mouseenter', element => {
+        console.log(`This element: ${element.type}`)
+        element.target.style.color = "red";
+    });
+    // element[0].target;
+});
+
+introContent.addEventListener('click', element => {
+    console.log(`This is ${element.type}`);
+})
+
+h2Headers.forEach(element => {
+    element.addEventListener('wheel', element => {
+        console.log(`${element.target}`);
+    })
+})
+
+h4Headers.forEach(element => {
+    element.addEventListener('select', element => {
+        console.log(`This element ${element.type}`);
+    })
+})
+
+middleContent.forEach(element => {
+    element.addEventListener('blur', element => {
+    
+})
+
+// button.forEach(element => {
+//     // element.addEventListener('mousemove', element => {
+//     //     console.log(`This element ${element.type}`)
+//     // })
+// })
